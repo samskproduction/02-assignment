2ef29fa245e779e869ecfa4f59b8356adf624f03
samskproduction <163208789+samskproduction@users.noreply.github.com>
2024-10-03 14:40:03 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link'
 import React from 'react'
-
 const footer = () => {
   return (
     <footer className='fixed justify-center items-center bottom-0 w-[100%] p-[40px 0] bg-black'>

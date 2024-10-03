537a46c1dd5e0cd4f0ece81dadd96b67cf56cd23
samskproduction <163208789+samskproduction@users.noreply.github.com>
2024-10-03 14:10:37 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,15 +4,7 @@ import React from 'react'
 const footer = () => {
   return (
     <footer className='fixed justify-center items-center bottom-0 w-[100%] p-[40px 0] bg-black'>
-        <ul className=" list-inside flex justify-center items-center mt-10  p-0 text-lg gap-4 sm:gap-6 md:gap-10 mb-0 text-center text-white">
-            <li className='cursor-pointer ease-in duration-300 hover:text-[#569DAA] '><Link href="/">Home</Link></li>
-            <li className='cursor-pointer ease-in duration-300 hover:text-[#569DAA]'><Link href="/portfolio">Portfolio</Link></li>
-            <li className='cursor-pointer ease-in duration-300 hover:text-[#569DAA]'><Link href="/contact">Contact</Link></li>
-            <li className='cursor-pointer ease-in duration-300 hover:text-[#569DAA]'><Link href="/about">About</Link></li>
-      </ul>
-
       <p className="copyright mt-10 text-center text-base mb-6 text-white">@Sameer Shahzad | All Rights Reserved</p>
-        
       </footer>
   )
 }

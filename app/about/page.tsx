697c7e9f960a8a12a7f8602c8785baf4fb97cfbf
samskproduction <163208789+samskproduction@users.noreply.github.com>
2024-10-03 14:42:03 +0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,6 @@ export default function About(){
         <div className="h-[80vh] ">
             <Header/>
            <h1 className="text-center mt-20 text-6xl font-bold">About</h1>
-           <p className="text-center mt-24 text-4xl ">I am a frontend developer passionate about creating innovative solutions and learning new technologies.</p>
            <Footer/>
         </div>
     )

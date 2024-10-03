5c03ed194b7140345de0f7d1de4b4ad5200bb966
samskproduction <163208789+samskproduction@users.noreply.github.com>
2024-10-03 14:12:58 +0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const footer = () => {
   return (
     <footer className='fixed justify-center items-center bottom-0 w-[100%] p-[40px 0] bg-black'>
+      
       <p className="copyright mt-10 text-center text-base mb-6 text-white">@Sameer Shahzad | All Rights Reserved</p>
       </footer>
   )

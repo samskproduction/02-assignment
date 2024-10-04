feddc04d96a5d8009ae33ff49922adc8b04b5f4d
samskproduction <163208789+samskproduction@users.noreply.github.com>
2024-10-04 23:29:13 +0500

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="w-full  overflow-x:hidden">
         <nav className="w-full sm:w-auto bg-black overflow-x:hidden py-8 px-[5%]">
-          <ul className='flex flex-wrap justify-center sm:justify-end gap-4 text-white text-lg  font-bold '>
+          <ul className='flex flex-wrap justify-center sm:justify-end gap-4 text-white text-lg sm:gap-6 md:gap-10 sm:text-xl md:text-2xl font-bold '>
             <li><Link href="/">Home</Link></li>
             <li><Link href="/portfolio">Portfolio</Link></li>
             <li><Link href="/contact">Contact</Link></li>

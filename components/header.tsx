eafaf43440cb7c0b734a6b5110625fc7e3e95fc4
samskproduction <163208789+samskproduction@users.noreply.github.com>
2024-10-04 23:34:41 +0500

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="w-full">
-        <nav className="w-full sm:w-auto bg-black overflow-x:hidden py-8 px-[5%]">
+    <div className="">
+        <nav className="w-full sm:w-auto bg-black py-8 px-[5%]">
           <ul className='flex flex-wrap justify-center sm:justify-end gap-4 text-white text-lg sm:gap-6 md:gap-10 sm:text-xl md:text-2xl font-bold '>
             <li><Link href="/">Home</Link></li>
             <li><Link href="/portfolio">Portfolio</Link></li>
